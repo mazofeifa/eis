ff073d1348f1ae351425874d532d1d98c78aaf48
Manuel Azofeifa <mazofeifa@advanceme.com>
2013-09-02 07:07:59 -0600

    individual

--- eis/Filters/InitializeSimpleMembershipAttribute.cs
+++ eis/Filters/InitializeSimpleMembershipAttribute.cs
@@ -4,7 +4,6 @@ using System.Data.Entity.Infrastructure;
 using System.Threading;
 using System.Web.Mvc;
 using WebMatrix.WebData;
-using eis.Models;
 
 namespace eis.Filters
 {
@@ -24,26 +23,7 @@ namespace eis.Filters
         private class SimpleMembershipInitializer
         {
             public SimpleMembershipInitializer()
-            {
-                Database.SetInitializer<UsersContext>(null);
-
-                try
-                {
-                    using (var context = new UsersContext())
-                    {
-                        if (!context.Database.Exists())
-                        {
-                            // Create the SimpleMembership database without Entity Framework migration schema
-                            ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
-                        }
-                    }
-
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
-                }
-                catch (Exception ex)
-                {
-                    throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
-                }
+            {               
             }
         }
     }
